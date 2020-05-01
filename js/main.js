1aa4ae71f7b89256cc3976d1b55d42115036f391
CaptainBorgman <isabelle.borgman@gmail.com>
2020-05-01 21:51:33 +0200

    Added index.html and main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// GLOBAL VARIABLES
+const quizContainer = document.getElementById('quiz-wrapper');
+const resultsContainer = document.getElementById('quiz-results');
+const submitButton = document.getElementById('submit-quiz');
+
+// FUNCTION: BUILD THE QUIZ
+function buildQuiz(){}
+
+
+// FUNCTION: SHOW QUIZ RESULTS
+function showResults(){}
+
+
+// DISPLAY QUIZ RIGHT AWAY
+buildQuiz();
+
+
+// ON SUBMIT, SHOW QUIZ RESULTS
+submitButton.addEventListener('click', showResults);
